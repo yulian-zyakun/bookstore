389d4dfbd12c7b721e8e08dcaadfbece6451d9cf
Yulian Zyakun <yzyakun@academic.rrc.ca>
2016-11-12 15:10:08 -0600

    Added permit parameters to ActiveAdmin.

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Customer do
-
+  permit_params :full_name, :address, :city, :postal_code, :email, :phone_number, :notes
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Order do
-
+  permit_params :status, :pst_rate, :gst_rate
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
