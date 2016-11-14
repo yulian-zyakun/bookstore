47826c248bf3e473790e48500f2316c10dccd44b
Yulian Zyakun <yzyakun@academic.rrc.ca>
2016-11-14 13:22:18 -0600

    Added books for the home page and ganre categories in ActiveAdmin.

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Book do
-  permit_params :name, :price, :image, :stock_quantity, :description
+  permit_params :name, :price, :image, :stock_quantity, :description, :category_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
   validates :name, :description, :stock_quantity, :price, presence: true
   validates :name, uniqueness: true, length: {minimum: 7}
-  
+
   has_many :line_items
+  belongs_to :category
   has_many :orders, :through => :line_items
 end
