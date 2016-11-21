d7f17f63a08304daa687f7b59d2e88182df67f39
Yulian Zyakun <yzyakun@academic.rrc.ca>
2016-11-21 10:14:45 -0600

    Adjusted before_action in the books controller.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,11 @@
 class BooksController < ApplicationController
+  before_action :load_categories
 
   def index
-    @categories = Category.all
     @books = Book.page(params[:page]).per(3)
   end
 
   def search
-    @categories = Category.all
     @books = Book.all
     if params[:search]
       @books = Book.search(params[:search]).order("created_at DESC").page(params[:page]).per(3)
@@ -16,14 +15,17 @@ class BooksController < ApplicationController
   end
 
   def show
-    @categories = Category.all
     @book = Book.find(params[:id])
   end
 
   def category
-    @categories = Category.all
     @category = Category.find(params[:id])
     @books = Book.where("category_id = ?", @category.id).page(params[:page]).per(3)
+  end
+
+  private
+  def load_categories
+    @categories = Category.all
   end
 
 end
